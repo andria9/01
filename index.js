dfa9427bd84cf086245ca89a258292b134bd9886
andria9 <41643936+andria9@users.noreply.github.com>
2025-05-30 07:38:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ client.on('message', async (message) => {
   try {
     const accessToken = await getAccessToken();
 
-    const webhookResponse = await fetch('https://hook.eu2.make.com/72e5lgtu75z2944684noafato89x1pmz', {
+    const webhookResponse = await fetch('https://hook.eu2.make.com/obp63jje60cabivmhqopxjj2j7lnwm1p', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
